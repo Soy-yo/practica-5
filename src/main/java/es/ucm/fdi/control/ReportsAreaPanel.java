91c8c11774016f0df9d9043a5ee1f833a57a6a3a
Pablo <pasanz10@ucm.es>
2018-04-23 18:04:27 +0200

    Merge Pablo y Edu

--- src/main/java/es/ucm/fdi/control/ReportsAreaPanel.java
+++ src/main/java/es/ucm/fdi/control/ReportsAreaPanel.java
@@ -3,11 +3,9 @@ package es.ucm.fdi.control;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import javax.swing.border.TitledBorder;
-
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.nio.file.Files;
 
 public class ReportsAreaPanel extends JScrollPane {
@@ -18,21 +16,17 @@ public class ReportsAreaPanel extends JScrollPane {
     super();
     initialize(minimumSize);
   }
-
-  public void clear() {
-	  area.setText("");
-  }
-  
-  public void saveToFile(File file) throws IOException {
-	  Files.write(file.toPath(), area.getText().getBytes("UTF-8"));
-  }
   
   private void initialize(Dimension size) {
     area = new JTextArea();
-    //area.setEditable(false);
+    area.setEditable(false);
     setBorder(new TitledBorder(new LineBorder(Color.BLACK), "Reports"));
     setMinimumSize(size);
     setViewportView(area);
+  }
+
+  public void saveToFile(File file) throws IOException {
+    Files.write(file.toPath(), area.getText().getBytes("UTF-8"));
   }
 
   public void clear() {
